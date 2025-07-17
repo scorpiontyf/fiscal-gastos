d8d4b38b48249e1450847e7d612314599a78f9ee
gcanechia <gcanechia@provertec.com.br>
2025-07-17 11:53:32 -0300

    ajustes no formulário.

--- src/pages/relatorio/create.tsx
+++ src/pages/relatorio/create.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-
+import { ShieldExclamationIcon } from "@heroicons/react/24/outline";
 type Inputs = {
   valor: number;
   descricao: string;
@@ -12,14 +12,11 @@ export default function CreateRelatorio() {
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-  // const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
-
-  console.log(watch("valor"));
-
+  
   return (
     <form
       className="px-4 py-8 sm:px-6 lg:px-8"
-      onSubmit={handleSubmit((data) => console.log(data))}
+      onSubmit={handleSubmit((data) => console.log("valores:", data))}
     >
       <div className="space-y-12">
         <div className="border-b border-white-900/10 pb-12">
@@ -32,39 +29,59 @@ export default function CreateRelatorio() {
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label
-                htmlFor="first-name"
-                className="block text-sm/6 font-medium text-white-900 text-start"
-              >
+              <label className="block text-sm/6 font-medium text-white-900 text-start">
                 Valor
               </label>
               <div className="mt-2">
                 <input
                   {...register("valor", { required: true })}
-                  id="first-name"
-                  name="first-name"
-                  type="text"
                   autoComplete="none"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-white-300 placeholder:text-white-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
+              {errors.valor && (
+                <p className="mt-2 text-sm text-start text-white-600">
+                  <div className="flex items-center gap-x-2">
+                    {watch("valor").toString().length < 1 ? (
+                      ""
+                    ) : (
+                      <ShieldExclamationIcon className="block size-6 group-data-open:hidden" />
+                    )}
+                    {errors.valor.message}
+                  </div>
+                </p>
+              )}
             </div>
 
             <div className="sm:col-span-3">
-              <label
-                htmlFor="last-name"
-                className="block text-sm/6 font-medium text-white-900 text-start"
-              >
+              <label className="block text-sm/6 font-medium text-white-900 text-start">
                 Descrição
               </label>
               <div className="mt-2">
                 <input
-                  {...register("valor", { required: true })}
-                  id="last-name"
-                  name="last-name"
+                  {...register("descricao", {
+                    required: true,
+                    minLength: {
+                      value: 10,
+                      message: "Descrição deve ter pelo menos 10 caracteres",
+                    },
+                  })}
+                  autoComplete="none"
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-white-300 placeholder:text-white-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 />
               </div>
+              {errors.descricao && (
+                <p className="mt-2 text-sm text-start text-white-600">
+                  <div className="flex items-center gap-x-2">
+                    {watch("descricao").length < 1 ? (
+                      ""
+                    ) : (
+                      <ShieldExclamationIcon className="block size-6 group-data-open:hidden" />
+                    )}
+                    {errors.descricao.message}
+                  </div>
+                </p>
+              )}
             </div>
           </div>
         </div>
@@ -75,13 +92,13 @@ export default function CreateRelatorio() {
           type="button"
           className="text-sm/6 font-semibold text-white-900"
         >
-          Cancel
+          Cancelar
         </button>
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Save
+          Salvar
         </button>
       </div>
     </form>
